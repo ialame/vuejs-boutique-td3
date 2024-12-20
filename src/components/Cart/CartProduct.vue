dca2b215dd41bd67d27a332bc57d86b0bfa50d03
MBP <fx.colombani@pcagrade.fr>
2024-12-20 12:25:40 +0100

    first commit

---
+++ src/components/Cart/CartProduct.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mb-10 b5">
+    <h1>Product</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Shop/ShopProduct.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="b5">
+    <h1>Shop Product</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
