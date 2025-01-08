00cbf3ac107f9850cfeec13eb4941ba7eb301eae
MBP <fx.colombani@pcagrade.fr>
2025-01-08 17:38:41 +0100

    images et data

--- src/data/product.ts
+++ src/data/product.ts
@@ -1,66 +1,59 @@
-export default [
+export default  [
   {
     id: 1,
-    image: "src/assets/images/macbookpro.PNG",
-    title: "Macbook Pro",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis",
-    price: 1500,
+    title : 'Alakazam',
+    image: 'src/assets/images/1.jpg',
+    price  : 25,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
     id: 2,
-    image: "src/assets/images/levono.PNG",
-    title: "Levono Pro",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis",
-    price: 2300,
+    title : 'Tortank',
+    image: 'src/assets/images/2.jpg',
+    price  : 35,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
     id: 3,
-    image: "src/assets/images/rider.PNG",
-    title: "Rider",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis",
-    price: 1200,
+    title : 'Leveinard',
+    image: 'src/assets/images/3.jpg',
+    price  : 45,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
     id: 4,
-    image: "src/assets/images/ldlc.PNG",
-    title: "LDLC benolo",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis",
-    price: 4500,
+    title : 'Dracaufeu',
+    image: 'src/assets/images/4.jpg',
+    price  : 55,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
     id: 5,
-    image: "src/assets/images/asus.PNG",
-    title: "Asus gamer",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis",
-    price: 3755,
+    title : 'Mélofée',
+    image: 'src/assets/images/5.jpg',
+    price  : 65,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
     id: 6,
-    image: "src/assets/images/rog.PNG",
-    title: "Rog desktop",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis",
-    price: 2452,
+    title : 'Léviator',
+    image: 'src/assets/images/6.jpg',
+    price  : 65,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
     id: 7,
-    image: "src/assets/images/msi.PNG",
-    title: "MSI play",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis",
-    price: 1478,
+    title : 'Tygnon',
+    image: 'src/assets/images/7.jpg',
+    price  : 75,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
   {
     id: 8,
-    image: "src/assets/images/pad.PNG",
-    title: "Think pad",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis",
-    price: 899,
+    title : 'Mackogneur',
+    image: 'src/assets/images/8.jpg',
+    price  : 85,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
   },
-];
+
+]
